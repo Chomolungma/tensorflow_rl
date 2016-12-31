212fb6a6e3d921487a7e3ed25f0df94ef7585b49
Pierre H. Richemond <Pierre H. Richemond>
2016-12-31 01:49:47 +0000

    first version with two test files.

---
+++ actor_critic.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 31 01:47:49 2016
+
+@author: Pierre H. Richemond
+"""
+
+import tensorflow as tf
+import prettytensor as pt
+
+import numpy as np
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+
+np.random.seed(1337)
+
+#TODO : actor-critic code here.
---
+++ deep_q_network.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 31 01:45:33 2016
+
+@author: Pierre H. Richemond
+"""
+import tensorflow as tf
+import prettytensor as pt
+
+import numpy as np
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+
+np.random.seed(1337)
+
+#TODO : insert deep Q network code here.
